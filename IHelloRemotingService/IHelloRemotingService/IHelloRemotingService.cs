c74fe08c549c053834aac3fb4113642289dafdf3
ÖmerAydoğan <96176210+OmerAydogan91@users.noreply.github.com>
2022-06-07 10:56:37 +0300

    Web Servis Kullanimi (Client/.NetRemotingService)
    Web Servis Kullanimi (Client/.NetRemotingService) arasındaki bağlantıyı sağlama (proje)

---
+++ IHelloRemotingService/IHelloRemotingService/IHelloRemotingService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+
+
+namespace IHelloRemotingService
+{
+    public interface IHelloRemotingService
+    {
+        
+        string GetMessage(string name);
+      
+
+
+    }
+}
---
+++ IHelloRemotingService/RemotingServiceHost/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemotingServiceHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            HelloRemotingService.HelloRemotingService remotingeService = new HelloRemotingService.HelloRemotingService();
+
+            TcpChannel channel = new TcpChannel(8080);
+            ChannelServices.RegisterChannel(channel);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(HelloRemotingService.HelloRemotingService), "GetMessage", WellKnownObjectMode.Singleton);
+            Console.WriteLine("Remoting Service Starded @"+DateTime.Now);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
